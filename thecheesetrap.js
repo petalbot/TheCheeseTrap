bbb21cbc5c8c5366efb154e7d23365878feeacf2
zhayun3233 <76890077+zhayun3233@users.noreply.github.com>
2022-02-04 17:27:31 -0500

    Add files via upload

---
+++ thecheesetrap.js
@@ -0,0 +1,36 @@
+background(10, 135, 255);
+
+//cheese
+fill(245, 175, 20);
+triangle (190, 150, 250, 20, 100, 20);
+
+//cheese holes
+fill(240, 214, 14);
+ellipse(187, 118, 20, 20);
+ellipse(130, 35, 20, 20);
+ellipse(225, 35, 20, 20);
+ellipse(185, 75, 20, 20);
+
+//mouse-body, ear, and feet
+fill(170, 165, 165);
+ellipse(200, 200, 100, 50);
+ellipse(190, 180, 30, 40);
+ellipse(180, 230, 20, 30);
+ellipse(220, 230, 20, 30);
+
+//nose
+fill(235, 103, 171);
+ellipse(150, 200, 10, 10);
+
+//tail and ear
+fill(240, 161, 218);
+triangle(250, 200, 250, 190, 1500, 500);
+ellipse(190, 180, 20, 30);
+
+//eye
+fill(8, 8, 8);
+ellipse(160, 190, 10, 10);
+
+var MoveMouseWhenItIsClickedOnAndDragged=function(){
+    
+};
